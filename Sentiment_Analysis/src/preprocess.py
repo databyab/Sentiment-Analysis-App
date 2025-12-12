82e4da4ab9327b1c415934e04d7df86bc93cecc4
databyab <aayushbhatt.data@gmail.com>
2025-12-12 16:49:39 +0530

    Update preprocess.py

--- Sentiment_Analysis/src/preprocess.py
+++ Sentiment_Analysis/src/preprocess.py
@@ -1,26 +1,25 @@
 import re
 import string
-import nltk
+from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 
 
 try:
-    from nltk.corpus import stopwords
     stop_words = set(stopwords.words("english"))
-except LookupError:
+except:
+    import nltk
     nltk.download("stopwords")
-    from nltk.corpus import stopwords
     stop_words = set(stopwords.words("english"))
-    
-lemmatizer = WordNetLemmatizer()
+
+stemmer = PorterStemmer()
 
 def clean_text(text):
     text = text.lower()
-    text = re.sub(r"http\S+|www.\S+", "", text)
+    text = re.sub(r"http\S+|www\S+", "", text)
     text = re.sub(r"@\w+|#\w+", "", text)
     text = re.sub(r"\d+", "", text)
     text = text.translate(str.maketrans("", "", string.punctuation))
     text = re.sub(r"\s+", " ", text).strip()
-    words = [lemmatizer.lemmatize(w) for w in text.split() if w not in stop_words]
+
+    words = [stemmer.stem(w) for w in text.split() if w not in stop_words]
     return " ".join(words)
